27d934615b1f391c21394e9c6db3f9f529c45ed4
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-05-01 20:33:53 -0500

    Added ProgramadorViajes class.
    + Created fechasSeSuperponen method to check if the dates are identical.
    + Created agregarDestinoPila method to add destinations to a stack.
    + Created agregarDestinoCola method to add destinations to a queue.

--- vacacionesPilasYColas.cpp
+++ vacacionesPilasYColas.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-class Destino {
+class Destino{
 public:
     string nombre;
     string fechaInicio;
@@ -22,3 +22,57 @@ public:
         actividades.push_back(actividad);
     }
 };
+
+class ProgramadorViajes{
+    public:
+    stack<Destino> pilaDestinos;
+    queue<Destino> colaDestinos;
+    set<string> destinosAgregados;
+
+    bool fechasSeSuperponen(Destino d1, Destino d2){
+        return d1.fechaInicio <= d2.fechaFin && d1.fechaFin >= d2.fechaInicio;
+    }
+
+    void agregarDestinoPila(Destino destino){
+        stack<Destino> pilaTemp = pilaDestinos;
+        while (!pilaTemp.empty()){
+            if(fechasSeSuperponen(pilaTemp.top(), destino)){
+                cout<<"Las fechas se superponen con un viaje existente."<<endl;
+                return;
+            }
+            pilaTemp.pop();
+        }
+
+        if(destinosAgregados.find(destino.nombre) == destinosAgregados.end()){
+            pilaDestinos.push(destino);
+            destinosAgregados.insert(destino.nombre);
+        }else{
+            cout<<"El destino ya ha sido agregado."<<endl;
+        }
+    }
+
+    void agregarDestinoCola(Destino destino) {
+        queue<Destino> colaTemp = colaDestinos;
+        while(!colaTemp.empty()){
+            if(fechasSeSuperponen(colaTemp.front(), destino)){
+                cout<<"Las fechas se superponen con un viaje existente."<<endl;
+                return;
+            }
+            colaTemp.pop();
+        }
+
+        if(destinosAgregados.find(destino.nombre) == destinosAgregados.end()){
+            colaDestinos.push(destino);
+            destinosAgregados.insert(destino.nombre);
+        }else{
+        cout<<"El destino ya ha sido agregado."<<endl;
+        }
+    }
+};
+
+int main(){
+
+    cout<<"Prueba"<<endl;
+
+    return 0;
+}
