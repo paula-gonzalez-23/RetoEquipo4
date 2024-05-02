0722633ebd7cc03549573a8634be7f5c77e2a97f
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-05-01 20:53:03 -0500

    Added Familia class.
    + Created agregarMiembro method to add a member to the family.
    + Created agregarDestinoMiembro method to add a destination to a family member.
    + Created consultarDestinosMiembro method to print the destinations of family member.
    + Created miembrosVisitandoDestino method to print who are the family members who will go to a specific destination.
    + Created obtenerActividadesDestino method to get the destination activities (to use).

--- vacacionesPilasYColas.cpp
+++ vacacionesPilasYColas.cpp
@@ -70,6 +70,99 @@ class ProgramadorViajes{
     }
 };
 
+class Familia{
+    public:
+    map<string, ProgramadorViajes> miembros;
+
+    void agregarMiembro(string nombre, ProgramadorViajes programador){
+        miembros[nombre] = programador;
+    }
+
+    void agregarDestinoMiembro(string nombre, Destino destino, bool pilaCola){
+        if(miembros.find(nombre) != miembros.end()){
+            if(pilaCola){
+                miembros[nombre].agregarDestinoPila(destino);
+            }else{
+                miembros[nombre].agregarDestinoCola(destino);
+            }
+        }else{
+            cout<<"Miembro no encontrado."<<endl;
+        }
+    }
+
+    void consultarDestinosMiembro(string nombre){
+        if (miembros.find(nombre) != miembros.end()){
+            ProgramadorViajes programador = miembros[nombre];
+            cout<<"Destinos en pila para "<<nombre<<":"<<endl;
+            while(!programador.pilaDestinos.empty()){
+                cout<<programador.pilaDestinos.top().nombre<<endl;
+                programador.pilaDestinos.pop();
+            }
+            cout<<"Destinos en cola para "<<nombre<<":"<<endl;
+            while(!programador.colaDestinos.empty()){
+                cout<<programador.colaDestinos.front().nombre<<endl;
+                programador.colaDestinos.pop();
+            }
+        }else{
+            cout<<"Miembro no encontrado."<<endl;
+        }
+    }
+
+    vector<string> miembrosVisitandoDestino(string nombreDestino){
+        vector<string> resultados;
+        for(auto& par : miembros){
+            ProgramadorViajes programador = par.second;
+            stack<Destino> pila = programador.pilaDestinos;
+            while(!pila.empty()){
+                if(pila.top().nombre == nombreDestino){
+                    resultados.push_back(par.first);
+                    break;
+                }
+                pila.pop();
+            }
+            if(!resultados.empty() && resultados.back() == par.first) continue;
+            queue<Destino> cola = programador.colaDestinos;
+            while(!cola.empty()){
+                if(cola.front().nombre == nombreDestino){
+                    resultados.push_back(par.first);
+                    break;
+                }
+                cola.pop();
+            }
+        }
+        return resultados;
+    }
+
+    vector<string> obtenerActividadesDestino(string nombreDestino){
+        vector<string> actividades;
+        for(auto& par : miembros){
+            ProgramadorViajes programador = par.second;
+            stack<Destino> pila = programador.pilaDestinos;
+            while(!pila.empty()){
+                if(pila.top().nombre == nombreDestino){
+                    for(const string& actividad : pila.top().actividades){
+                        actividades.push_back(actividad);
+                    }
+                    break;
+                }
+                pila.pop();
+            }
+            if(!actividades.empty()) continue;
+            queue<Destino> cola = programador.colaDestinos;
+            while(!cola.empty()){
+                if(cola.front().nombre == nombreDestino){
+                    for(const string& actividad : cola.front().actividades){
+                        actividades.push_back(actividad);
+                    }
+                    break;
+                }
+                cola.pop();
+            }
+        }
+        return actividades;
+    }
+};
+
 int main(){
 
     cout<<"Prueba"<<endl;
